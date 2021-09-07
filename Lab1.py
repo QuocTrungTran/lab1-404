4a68f8bc3a08d5d6d45d9c540d900dd912d582c8
Trung <quoctrun@ualberta.ca>
2021-09-06 18:29:20 -0600

    Update Step 10

--- Lab1.py
+++ Lab1.py
@@ -3,3 +3,8 @@ import requests
 print(version('requests'))
 r = requests.get('http://google.com')
 print(r.status_code)
+
+
+r1 =  requests.get('https://raw.githubusercontent.com/QuocTrungTran/lab1-404/master/Lab1.py?flush_cache=true')
+print(r1.text)
+
