534fad18138ebc06e3fbb40bf462bfe6c3d20b7f
Trung <quoctrun@ualberta.ca>
2021-09-10 12:38:13 -0600

    Add comment

--- Lab1.py
+++ Lab1.py
@@ -1,10 +1,11 @@
 from importlib.metadata import version
 import requests
-print(version('requests'))
+print('Version of requests: ' + version('requests')) #display version of requests library
+
 r = requests.get('http://google.com')
 print(r.status_code)
 
-
+print('-----------------------------------')
 r1 =  requests.get('https://raw.githubusercontent.com/QuocTrungTran/lab1-404/master/Lab1.py?flush_cache=true')
 print(r1.text)
 
